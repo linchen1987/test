27ed1376f3e2ee9444e40e0a099d966259d207bf
linchen1987 <linchen.1987@foxmail.com>
2020-11-10 10:27:52 +0800

    Update node.js

--- tools/node.js
+++ tools/node.js
@@ -1,7 +1,7 @@
 console.log(1111)
 
-const { Octokit } = require("@octokit/core");
+// const { Octokit } = require("@octokit/core");
 
-console.log(typeof Octokit)
+// console.log(typeof Octokit)
 
 // const octokit = new Octokit({ auth: `personal-access-token123` });
