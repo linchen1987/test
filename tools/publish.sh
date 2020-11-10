648220cb88740013960d7b89ccabea351866c02e
linchen1987 <linchen.1987@foxmail.com>
2020-11-10 10:00:15 +0800

    Update publish.sh

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,7 +1,7 @@
 echo who
 whoami
-sudo su
-whoami
+# sudo su
+# whoami
 
 echo "node version"
 node -v
@@ -9,17 +9,17 @@ echo ""¸
 
 echo "gem"
 type gem
-sudo gem install travis
-type travis
-echo ""¸
+# sudo gem install travis
+# type travis
+# echo ""¸
 
-echo 'install @abtnode/cli'
-type npm
-npm bin -g
-sudo npm install -g @abtnode/cli
+# echo 'install @abtnode/cli'
+# type npm
+# npm bin -g
+# sudo npm install -g @abtnode/cli
 
-echo abtnode version
-echo $PATH
-abtnode -V
+# echo abtnode version
+# echo $PATH
+# abtnode -V
 
 node tools/node.js
